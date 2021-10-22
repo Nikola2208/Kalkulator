6fba7a2542299c2b4adb3d37e863e194ed59c725
Nikola Holjevac <holja2208@gmail.com>
2021-10-22 15:34:13 +0200

    initial read loop

--- src/kalkulator/Main.java
+++ src/kalkulator/Main.java
@@ -1,10 +1,17 @@
 package kalkulator;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner scanner;
+		scanner=new Scanner(System.in);
+		String string;
+		do {
+			string = scanner.nextLine();
+		}while(!string.equals("kraj"));
 	}
 
 }
