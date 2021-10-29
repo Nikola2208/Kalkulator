1e118775818fd8e25532056757aca58f7c33afd3
Nikola Holjevac <holja2208@gmail.com>
2021-10-29 14:07:10 +0200

    implement substraction

--- src/kalkulator/Main.java
+++ src/kalkulator/Main.java
@@ -23,10 +23,15 @@ public class Main {
 			}
 			int x = Integer.parseInt(parts[0]);
 			int y = Integer.parseInt(parts[2]);
-			if (parts[1].equals("+")) {
-				System.out.println(x+y);
+			if (parts[1].equals("-")) {
+				substraction(x,y);
 			}
 		}while(!string.equals("kraj"));
 	}
 
+	public static void substraction(int x, int y) {
+		System.out.println(x-y);
+	}
+	
 }
+
